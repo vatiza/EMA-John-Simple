04901891bc706803d2eb978b134402101ded8206
vatiza <rakibur.mac@gmail.com>
2024-02-01 01:11:49 +0600

    load fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Header from './Components/Header/Header'
+import Shop from './Components/Shop/Shop'
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
      <Header></Header>
+     <Shop></Shop>
     
       
       
---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProducts(data));
+    }, [])
+
+
+    return (
+        <div className='shop-container'>
+            <div className='product-container'>
+                <h2>Product Comming Heare:{products.length}</h2>
+
+            </div>
+
+            <div className='cart-container'>
+                <h4>Order Summary</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
