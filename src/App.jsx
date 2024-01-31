45367c236f2815a5c264885515564122928f9002
vatiza <rakibur.mac@gmail.com>
2024-02-01 00:20:06 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      git init
+      git add .
+      git commit - m "first commit"
+      git branch - M main
+      git remote add origin https://github.com/vatiza/EMA-John-Simple.git
+      git push - u origin main
+
+    </>
+  )
+}
+
+export default App
