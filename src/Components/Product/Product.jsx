90829e1b95c8ac33bcf3b34849951b23596fe043
vatiza <rakibur.mac@gmail.com>
2024-02-01 02:13:30 +0600

    product container desing css recap

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -3,10 +3,17 @@ import './Product.css';
 
 const Product = (props) => {
     console.log(props.product);
-    const { img, name, seller, quantity,price } = props.product;
+    const { img, name, seller, ratings, quantity, price } = props.product;
     return (
         <div className="product">
             <img src={img} alt="" />
+            <div className='product-info'>
+                <h2 className='product-name'>{name}</h2>
+                <p className='product-price'> Price: ${price}</p>
+                <p className='product-manufacturer'>Manufacturer: {seller}</p>
+                <p className='product-rating'>Rating: {ratings}</p>
+            </div>
+            <button className='addtocart-btn'>Add To Cart <i class="fa-solid fa-cart-arrow-down"></i></button>
 
         </div>
     );
