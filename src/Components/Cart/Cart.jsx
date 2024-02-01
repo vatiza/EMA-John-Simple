2828284eaf182881ee65dcbd68ca1eda1ca5bf15
vatiza <rakibur.mac@gmail.com>
2024-02-02 02:43:56 +0600

    toFixed decimal

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -19,8 +19,8 @@ const Cart = ({ cart }) => {
             <p>Selected Items: {cart.length}</p>
             <p>Total Price:$ {total} </p>
             <p>Total Shipping:$ {totalShipping}</p>
-            <p>Tax:$ {tax}</p>
-            <h6>Grand Total:$ {grandTotal}</h6>
+            <p>Tax:$ {tax.toFixed(2)}</p>
+            <h6>Grand Total:$ {grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
