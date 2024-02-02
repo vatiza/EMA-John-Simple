956754e7c01599405b78401e1b7136c5650615ff
vatiza <rakibur.mac@gmail.com>
2024-02-03 02:21:15 +0600

    Selected Items:

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -4,12 +4,15 @@ import './Cart.css';
 const Cart = ({ cart }) => {
     // const cart = props.cart;
     // const { cart } = props;
-console.log(cart)
+    console.log(cart)
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-        total = total + product.price;
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = total * 4 / 100;
     const grandTotal = total + totalShipping + tax;
@@ -17,7 +20,7 @@ console.log(cart)
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price:$ {total} </p>
             <p>Total Shipping:$ {totalShipping}</p>
             <p>Tax:$ {tax.toFixed(2)}</p>
