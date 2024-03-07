5451a4d23c54df04a897f90bc73f2dfab7c22b53
vatiza <rakibur.mac@gmail.com>
2024-03-07 22:53:19 +0600

    delete product form order review page

--- src/Components/OrderReview/OrderReview.jsx
+++ src/Components/OrderReview/OrderReview.jsx
@@ -1,8 +1,8 @@
 import { TrashIcon } from '@heroicons/react/24/solid'
 import './OrderReview.css';
 
-const OrderReview = ({ product }) => {
-    const { img, category, name, price, quantity } = product;
+const OrderReview = ({ product, handleRemoveFromCart }) => {
+    const { img, id, category, name, price, quantity } = product;
     return (
         <div className='review-item'>
             <img src={img} alt="" />
@@ -11,7 +11,7 @@ const OrderReview = ({ product }) => {
                 <p>Price:<span className='price-text'>$ {price}</span></p>
                 <p>Order Quantity : <span className='quantity-text'>{quantity}</span></p>
             </div>
-            <button className='delete-btn'>
+            <button onClick={() => handleRemoveFromCart(id)} className='delete-btn'>
                 <TrashIcon className='trash-icon'></TrashIcon>
 
             </button>
--- src/Components/Orders/Orders.jsx
+++ src/Components/Orders/Orders.jsx
@@ -2,15 +2,24 @@ import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import OrderReview from "../OrderReview/OrderReview";
 import './Orders.css'
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { removeFromDb } from "../utilities/fakedb";
 
 
 
 const Orders = () => {
     const savedCart = useLoaderData();
-    const [cart, setCart] = useState([savedCart])
+    const [cart, setCart] = useState([savedCart]);
 
-    console.log(savedCart)
+
+    const handleRemoveFromCart = (id) => {
+        //! please fix this 17 number line after error: product is not defined;
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
+
 
     return (
         <div className='shop-container'>
@@ -19,6 +28,7 @@ const Orders = () => {
                     savedCart.map(product => <OrderReview
                         key={product.id}
                         product={product}
+                        handleRemoveFromCart={handleRemoveFromCart}
                     ></OrderReview>)
                 }
             </div>
