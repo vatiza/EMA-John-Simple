17b2357e0683f4dbedd6bbd4d3d2d8bbfa8af41c
vatiza <rakibur.mac@gmail.com>
2024-05-02 13:06:50 +0600

    change fetch location

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,72 +1,74 @@
-import { useEffect, useState } from 'react';
-import './Shop.css';
-import Product from '../Product/Product';
-import Cart from '../Cart/Cart';
-import { addToDb, deleteShoppingCart, getShoppingCart } from '../utilities/fakedb';
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCreditCard, faListCheck } from '@fortawesome/free-solid-svg-icons';
+import { faListCheck } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import {
+  addToDb,
+  deleteShoppingCart,
+  getShoppingCart,
+} from "../utilities/fakedb";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data));
-    }, []);
-    useEffect(() => {
-        const storedCart = getShoppingCart();
-        const savedCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find(product => product.id === id);
-            //get quantity of the product
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                //add the added product to saved cart
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
-    }, [products])
-
-    const [cart, setCart] = useState([])
-    const handleAddToCart = (product) => {
-        const newCart = [...cart, product];
-        setCart(newCart);
-        addToDb(product.id);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      //get quantity of the product
+      if (addedProduct) {
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        //add the added product to saved cart
+        savedCart.push(addedProduct);
+      }
     }
+    setCart(savedCart);
+  }, [products]);
 
-    const handleClearCart = () => {
-        setCart([]);
-        deleteShoppingCart();
-    }
+  const [cart, setCart] = useState([]);
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+    addToDb(product.id);
+  };
 
-    return (
-        <div className='shop-container'>
-            <div className='product-container'>
+  const handleClearCart = () => {
+    setCart([]);
+    deleteShoppingCart();
+  };
 
-                {
-                    products.map(product => <Product
-                        key={product.id}
-                        product={product}
-                        handleAddToCart={handleAddToCart}
-                    ></Product>)
-                }
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
 
-            </div>
-
-            <div className='cart-container'>
-                <Cart cart={cart}
-                    handleClearCart={handleClearCart} >
-
-                    <Link to="/orders">
-                        <button className='checkout-btn'>Review Order <FontAwesomeIcon icon={faListCheck} /></button>
-                    </Link>
-                </Cart>
-            </div>
-        </div >
-    );
+      <div className="cart-container">
+        <Cart cart={cart} handleClearCart={handleClearCart}>
+          <Link to="/orders">
+            <button className="checkout-btn">
+              Review Order <FontAwesomeIcon icon={faListCheck} />
+            </button>
+          </Link>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
